9c9989b3b59d6c78064e50639cb1417b7a51091e
Iván Álvarez Fernández <124284729+ivanaf03@users.noreply.github.com>
2024-01-30 19:23:13 +0100

    Delete p1 directory

--- p1/ipm-2324-p1-grupo_32-main/src/exceptions.py
+++
@@ -1,9 +0,0 @@
-class NotInternetException(Exception):
-    # Raised when there is no Internet connection
-    def __init__(self):
-        super().__init__()
-
-class NoResultsException(Exception):
-    # Raised when no results are found in a query
-    def __init__(self):
-        super().__init__()
